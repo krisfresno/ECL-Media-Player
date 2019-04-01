30595f158722f7331447e88eda46b1826a5e58d4
krisfresno <41947126+krisfresno@users.noreply.github.com>
2019-04-02 00:16:32 +0200

    Update index.js

--- media/routes/index.js
+++ media/routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 var fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+	const casa = ["photo1.jpg", "photo2.jpg"];
+	const casa1 = JSON.stringify(casa);
+
+  	res.render('index', { 
+  		list : JSON.stringify(casa),
+  	});
 });
 
 module.exports = router;
