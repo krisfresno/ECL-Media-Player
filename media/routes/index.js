cbc3e17da29dfc90408672d4f5e57be43cd48cde
krisfresno <41947126+krisfresno@users.noreply.github.com>
2019-04-02 21:06:29 +0200

    resume

--- media/routes/index.js
+++ media/routes/index.js
@@ -7,15 +7,10 @@ router.get('/', function(req, res) {
 	const casa = ["photo1.jpg", "photo2.jpg"];
 	const casa1 = JSON.stringify(casa);
 	const lista = [];
-	//var dir = '../public/images'+__dirname+'';
-	//var dir = "C:/Cosas/Proyectos/ECL-Media-Player/media/routes";
 	var dir = "./public/images/";
-	//console.log(dir);
 	fs.readdirSync(dir).forEach(file =>{
-		//console.log(file);
 		lista.push(file);
 	});
-	console.log(lista);
   	res.render('index', { 
   		list : JSON.stringify(lista),
   	});
